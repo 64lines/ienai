de417ad75ac10aaed3ae89a125de791cc634072d
Julian Alexander Murillo <julixrevolutions@gmail.com>
2013-10-09 15:35:48 -0500

    Cool new changes and add a new functionallity to FeedEk library

--- js/ienai.js
+++ js/ienai.js
@@ -7,43 +7,65 @@ function fixVisualConfiguration(size) {
     }
 }
 
-function showResources(listFeeds) {
-    listFeeds.sort(function() {return 0.5 - Math.random()});
-    
-    $.each(listFeeds, function(index, value) {
-        var newsUrl = document.location.host;
+function addShareButtons(parent) {
+    var titleElement = parent
+                    .children(".feedEkList")
+                    .children("li")
+                    .children(".itemTitle");
 
-        var twitterText = "Sharing news: " 
-            + value 
+    titleElement.append($("<br>"));
+     
+    titleElement.append(function() { 
+        var newsUrl = document.location.host;
+        var newTitle = $(this).children("a").text();
+        var value = $(this).children("a").attr("href");
+        var twitterText = "Sharing news: "
+            + newTitle
+            + " "
+            + value
             + " via "
             + newsUrl 
             + " " 
             + HASH_TAGS;
-
+ 
         var buttonLink = TWITTER_SHARE_TEXT + twitterText;
-
+     
         var iconButtonShare = $("<span>");
         iconButtonShare.addClass("icon-share-alt icon-white");
-
-        var iconButtonGo = $("<span>");
-        iconButtonGo.addClass("icon-globe icon-white");
-
+     
         var shareButton = $("<a>");
         shareButton.attr("href", buttonLink);
-        shareButton.attr("style", "margin: 5px 0 5px 0;");
+        shareButton.attr("style", "margin: 5px 0px 5px 0px;");
         shareButton.attr("title", "Share on twitter " + value);
         shareButton.attr("target", "_blank");
         shareButton.addClass("btn btn-primary btn-small share-button");
-        shareButton.append(iconButtonShare);
-
+        shareButton.append(iconButtonShare);    
+     
+        return shareButton;
+    });
+     
+    titleElement.append(function() { 
+        var iconButtonGo = $("<span>");
+        var value = $(this).children("a").attr("href");
+        iconButtonGo.addClass("icon-globe icon-white");
+     
         var gotToResourceButton = $("<a>");
         gotToResourceButton.attr("href", value);
-        gotToResourceButton.attr("style", "margin: 5px 0 5px 5px;");
+        gotToResourceButton.attr("style", "margin: 5px 5px 5px 5px;");
         gotToResourceButton.attr("title", "Go to " + value);
         gotToResourceButton.attr("target", "_blank");
         gotToResourceButton.addClass("btn btn-success btn-small share-button");
         gotToResourceButton.append(iconButtonGo);
+     
+        return gotToResourceButton;
+    });    
+}
 
+
+function showResources(listFeeds) {
+    listFeeds.sort(function() {return 0.5 - Math.random()});
+    
+    $.each(listFeeds, function(index, value) {
         var spanEntry = $("<span>");
         spanEntry.addClass("span3");
         spanEntry.attr("id", "news" + index);
@@ -51,9 +73,8 @@ function showResources(listFeeds) {
         var entry = $("<div>");
         entry.attr("id", "entry" + index);
         entry.attr("title", "Resource: " + value)
+        entry.addClass("newsPanel");
 
-        spanEntry.append(shareButton);
-        spanEntry.append(gotToResourceButton);
         spanEntry.append(entry);
 
         $("#entries").append(spanEntry);
@@ -63,6 +84,10 @@ function showResources(listFeeds) {
             MaxCount: 3,
             ShowPubDate: false,
             DescCharacterLimit: 1000,
+            TitleLinkTarget: '_blank',
+            Success: function() {
+                addShareButtons($("#entry" + index));
+            }
         });
     });
 }
